50fafa5c15d521fa8084fb9194c2b35f89a2e801
Faisal7007 <126740052+Faisal7007@users.noreply.github.com>
2023-06-09 18:29:52 +0530

    commit

---
+++ src/components/Details2.jsx
@@ -0,0 +1,72 @@
+
+import React, { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+import './Details.css'
+
+function Details() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [input,setInput] = useState('')
+
+  const fetchApi = async () => {
+    setLoading(true);
+    let fetchData = await fetch("https://jsonplaceholder.typicode.com/users");
+    fetchData = await fetchData.json();
+    setLoading(false);
+    console.log("User Data ", fetchData);
+    setData(fetchData);
+  };
+
+  useEffect(() => {
+    fetchApi();
+    
+  }, []);
+
+
+
+  const handleInput = (e)=>{
+    const term = e.target.value
+      setInput (term)
+    //   console.log(e.target.value)  
+    const updateData = data && data.filter((item)=>{
+     
+            return(
+
+            item.name.toLowerCase().includes(term.toLowerCase())||
+            item.name.toLowerCase().startsWith(term.toLowerCase())
+            )
+          
+          
+
+    })
+    setData(updateData)
+   
+      }
+   
+        
+       
+
+  return (
+    <div className="main">
+      <h1>UserNames</h1>
+
+      <input type="text" value={input}  onChange={handleInput} />
+
+      {loading ? <Spinner /> : ""}
+
+      {
+      data && data
+      .map((item) => (
+        <div   className="content" key={item.name}>
+            
+          <h4>{item.id}</h4>  
+
+          <h2 /*</div>onClick={()=>fillInput(item.name)}*/ >{item.name}</h2>
+           
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Details;
